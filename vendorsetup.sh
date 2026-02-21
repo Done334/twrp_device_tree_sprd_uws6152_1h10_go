5e94d91642bd80b90672d8c6f42617947588dcb0
Done334 <1069734236@qq.com>
2026-02-21 16:33:25 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-patch -Np1 < device/sprd/uws6152_1h10_go/patches/permissive-selinux.patch
+patch p0 < device/sprd/uws6152_1h10_go/patches/permissive-selinux.patch
 add_lunch_combo omni_uws6152_1h10_go-userdebug
 add_lunch_combo omni_uws6152_1h10_go-eng
